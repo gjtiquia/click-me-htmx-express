07a683d595f79492076fc6ab577857888ae12841
GJ Tiquia <gershom.tiquia@gmail.com>
2024-03-01 22:15:39 +0800

    feat: basic counter

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+// Static file server
 app.use(express.static("public"))
 
-app.get('/page', (req, res) => {
-    res.send('Hello World!')
+// Counter
+let counter = 50; // Starts from non-zero
+
+app.get('/get-counter', (req, res) => {
+    res.status(200).send(counter.toString());
+})
+
+app.post('/clicked', (req, res) => {
+    console.log("clicked")
+    counter++;
+    res.status(200).send(counter.toString());
 })
 
 app.listen(port, () => {
